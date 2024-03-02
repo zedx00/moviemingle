db3a1ff87f4500ef3ba008e479615e9f185cbaed
zedx00 <156818273+zedx00@users.noreply.github.com>
2024-03-02 12:19:03 +0500

    Add files via upload

---
+++ final project/main.js
@@ -0,0 +1,14 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.querySelector('.video');
+    video.load();
+});
